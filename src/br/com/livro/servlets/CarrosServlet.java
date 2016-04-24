10d9b13dfd3f2b4d77fde45229b8ec44d6ddd6f4
Gerson Cardoso Filho <gersoncfilho@mac.com>
2016-04-24 07:53:01 -0300

    Implementa POST e DELETE

--- src/br/com/livro/servlets/CarrosServlet.java
+++ src/br/com/livro/servlets/CarrosServlet.java
@@ -46,9 +46,7 @@ public class CarrosServlet extends HttpServlet {
 			String json = gson.toJson(carros);
 			ServletUtil.writeJSON(resp, json);	
 		}
-		
-		
-		
+	
 //		//Gera o xml
 //		String xml = JAXBUtil.toXML(lista);
 //		//Escreve o xml no response do servlet com application/xml
@@ -60,5 +58,49 @@ public class CarrosServlet extends HttpServlet {
 //		//Escreve o JSON no response do servlet com application/json
 //		ServletUtil.writeJSON(resp, json);		
 	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//Cria carro
+		Carro carro = getCarroFromRequest(req);
+		//Salva carro
+		carroservice.save(carro);
+		//Escreve o JSON do novo carro
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(carro);
+		ServletUtil.writeJSON(resp, json);	
+	}
+	
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String requestUri = req.getRequestURI();
+		Long id = RegexUtil.matchId(requestUri);
+		if(id!=null){
+			carroservice.delete(id);
+			resp.sendError(200,"Carro excluído com sucesso!");
+		}else{
+			resp.sendError(404,"URL inválida!");
+		}
+	}
+	
+	//Le os parametros da request e cria o objeto carro
+	private Carro getCarroFromRequest(HttpServletRequest request){
+		Carro c = new Carro();
+		String id = request.getParameter("id");
+		if(id!=null){
+			//Se informou o id, busca o objeto do banco de dados
+			c = carroservice.getCarro(Long.parseLong(id));
+		}
+		c.setNome(request.getParameter("nome"));
+		c.setDesc(request.getParameter("descricao"));
+		c.setUrlFoto(request.getParameter("url_foto"));
+		c.setUrlVideo(request.getParameter("url_video"));
+		c.setLatitude(request.getParameter("latitude"));
+		c.setLongitude(request.getParameter("longitude"));
+		c.setTipo(request.getParameter("tipo"));
+		return c;
+	}
+	
+	
+	
 	
 }
